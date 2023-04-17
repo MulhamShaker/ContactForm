cad1bcb5a70d136aa3d3f047e7e171ef83054d3b
molham-shaker <molham.sh95@gmail.com>
2023-04-17 21:22:34 +0200

    1

--- components/ContactPage.js
+++ components/ContactPage.js
@@ -18,41 +18,41 @@ function ContactPage() {
         <div className="md:w-1/2 w-full md:p-10 my-10 md:my-0  flex-col">
           <h1 className="text-4xl font-semibold text-white ">Say Hello ,</h1>
           <p className="text-gray-300 font-semibold text-sm my-5">
-            Fill up the form & we will get back to you within 12 hours.
+            Fill up the form & we will get back to you as fast as we can.
           </p>
           <div className="flex-col my-20 gap-5 flex  ">
-            <div className="flex gap-5 p-4 border border-gray-500 hover:border-blue-700 md:w-1/2 w-full rounded-lg group cursor-pointer items-center">
+            <div className="flex gap-5 p-4 border border-gray-500 hover:border-white md:w-1/2 w-full rounded-lg group cursor-pointer items-center">
               <BsTelephoneFill className="text-xl text-gray-500  group-hover:text-white" />
               <p className="text-gray-300 text-base font-semibold group-hover:text-blue-300 group-hover:scale-110">
-                +31686049025
+                +123456789
               </p>
             </div>
-            <div className="flex gap-5 p-4 border border-gray-500 hover:border-blue-700 md:w-1/2 w-full rounded-lg group cursor-pointer items-center">
+            <div className="flex gap-5 p-4 border border-gray-500 hover:border-white md:w-1/2 w-full rounded-lg group cursor-pointer items-center">
               <GrMail className="text-xl text-gray-500  group-hover:text-white" />
               <p className="text-gray-300 text-base font-semibold group-hover:text-blue-300 group-hover:scale-110">
-                molham.sh95@gmail.com
+                test@gmail.com
               </p>
             </div>
-            <div className="flex gap-5 p-4 border border-gray-500 hover:border-blue-700 md:w-1/2 w-full rounded-lg group cursor-pointer items-center">
+            <div className="flex gap-5 p-4 border border-gray-500 hover:border-white md:w-1/2 w-full rounded-lg group cursor-pointer items-center">
               <GrMail className="text-xl text-gray-500  group-hover:text-white" />
               <p className="text-gray-300 text-base font-semibold group-hover:text-blue-300 group-hover:scale-110">
-                molham.shaker@pandiv.com
+                test.test@pandiv.com
               </p>
             </div>
-            <div className="flex gap-5 p-4 border border-gray-500 hover:border-blue-700 md:w-1/2 w-full rounded-lg group cursor-pointer items-center">
+            <div className="flex gap-5 p-4 border border-gray-500 hover:border-white md:w-1/2 w-full rounded-lg group cursor-pointer items-center">
               <HiLocationMarker className="text-xl text-gray-500  group-hover:text-white" />
               <p className="text-gray-300 text-base font-semibold group-hover:text-blue-300 group-hover:scale-110">
-                EU , Netherlands
+                Earth
               </p>
             </div>
           </div>
 
           <div className="flex gap-8  justify-center md:justify-start ">
-            <FaInstagram className="text-2xl text-white hover:text-blue-700 cursor-pointer" />
-            <FaTwitter className="text-2xl text-white hover:text-blue-700 cursor-pointer" />
-            <FaYoutube className="text-2xl text-white hover:text-blue-700 cursor-pointer" />
-            <FaPinterest className="text-2xl text-white hover:text-blue-700 cursor-pointer" />
-            <FaGithub className="text-2xl text-white hover:text-blue-700 cursor-pointer" />
+            <FaInstagram href="#" className="text-2xl text-white hover:text-gray-400 cursor-pointer" />
+            <FaTwitter href="#" className="text-2xl text-white hover:text-gray-400 cursor-pointer" />
+            <FaYoutube href="#" className="text-2xl text-white hover:text-gray-400 cursor-pointer" />
+            <FaPinterest href="#" className="text-2xl text-white hover:text-gray-400 cursor-pointer" />
+            <FaGithub href="#" className="text-2xl text-white hover:text-gray-400 cursor-pointer" />
           </div>
         </div>
         {/* Contact form */}
@@ -104,7 +104,7 @@ function ContactPage() {
 
             <button
               type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-white bg-gray-400 hover:bg-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-pink-500 dark:hover:bg-purple-500 dark:focus:ring-blue-800"
             >
               Send Message
             </button>
